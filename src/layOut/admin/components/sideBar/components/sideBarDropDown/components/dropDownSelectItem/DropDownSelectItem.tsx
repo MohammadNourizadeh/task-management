7b06d16b7dc5fdf07c2dad6311bb07407cf6426f
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2024-07-23 16:33:46 +0330

    new dropDownItem created for select options

---
+++ src/layOut/admin/components/sideBar/components/sideBarDropDown/components/dropDownSelectItem/DropDownSelectItem.tsx
@@ -0,0 +1,27 @@
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './DropDownSelectItem.module.scss';
+
+type DropDownSelectItemProps = {
+    itemName: string;
+    icon: IconDefinition
+}
+
+export default function DropDownSelectItem({ itemName, icon }: DropDownSelectItemProps) {
+    return (
+        <li className={styles.dropDownItem}>
+            <span className={styles.iconContainer}>
+                <FontAwesomeIcon icon={icon} />
+            </span>
+            <span className={styles.itemNameContainer}>
+                {itemName}
+            </span>
+            <span className={styles.selectInputContainer}>
+                <select>
+                    <option value="darkMode">dark mode</option>
+                    <option value="lightMode">light mode</option>
+                </select>
+            </span>
+        </li>
+    )
+}
