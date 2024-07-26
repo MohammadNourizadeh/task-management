1d221bd7aa1968567d0c9e6f0e95b375611aedf3
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2024-07-26 22:47:48 +0330

    light mode style added for pageMessage file

--- src/components/pageMessage/PageMessage.tsx
+++ src/components/pageMessage/PageMessage.tsx
@@ -1,14 +1,18 @@
 import { IconDefinition } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './PageMessage.module.scss'
+import { useSelector } from 'react-redux'
 
 type ImportantTaskPageProps = {
     message: string,
     icon: IconDefinition
 }
 export default function PageMessage({ message, icon }: ImportantTaskPageProps) {
+    // redux
+    const mode = useSelector(store => store.darkAndLightMode.mode)
+
     return (
-        <div className={styles.king}>
+        <div className={styles.king} id={mode === 'lightMode' ? styles.lightMode : ''}>
             <span>{message}</span>
             <span>
                 <FontAwesomeIcon icon={icon} />
